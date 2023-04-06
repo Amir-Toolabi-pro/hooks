b47bd506d19d1226bebb98dbb652227b6208ab3b
Amir-Toolabi-pro <amirooogamer81@gmail.com>
2023-04-06 17:48:37 +0430

    useMemo

--- src/components/AllComponents.jsx
+++ src/components/AllComponents.jsx
@@ -2,16 +2,32 @@ import React from 'react';
 import RefExample from './RefExample';
 import MemoExample from './MemoExample';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Accordion } from 'react-bootstrap';
+
 
 const AllComponents = () => {
-  return ( 
+  return (
     <>
-      <RefExample/>
-      <br />
-      <hr />
-      <MemoExample/>
+      <h3 style={{textAlign:"center", margin:"10px 0 30px"}} >
+        یادگیری و استفاده از هوک ها
+      </h3>
+      <Accordion dir='rtl'>
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>useRef</Accordion.Header>
+          <Accordion.Body>
+            <RefExample />
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="1">
+          <Accordion.Header>useMemo</Accordion.Header>
+          <Accordion.Body>
+            <MemoExample />
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
     </>
-   );
+  );
 }
- 
+
 export default AllComponents;
--- src/components/MemoExample.jsx
+++ src/components/MemoExample.jsx
@@ -1,12 +1,49 @@
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
+
+const dubleNumber = (num)=>{
+  console.log("فانکشن تنبل");
+  let count = 0;
+  while(count<= 1000000000){
+    count++
+  };
+  return num * 2;
+}
 
 
 const MemoExample = () => {
-  return ( 
+
+  const [color,setColor] = useState(false);
+  const [number, setNumber] = useState(0);
+
+  const supernum = useMemo(()=>{
+    return dubleNumber(number);
+  },[number])
+
+  const appstyle = useMemo(()=>{
+    return {background: color? "green":"red"}
+  },[color])
+  
+
+  useEffect(()=>{
+    console.log("بکگراند تغییر کرد");
+  },[appstyle])
+
+  return (
     <>
-      
+      <h3 style={{ textAlign: "center", margin: "0 0 20px 0", color: "GrayText" }}>
+        تمرین هوک Memo
+      </h3>
+      <form onSubmit={(e) => e.preventDefault()} style={{ width: "60%", margin: "auto" }}>
+        <input value={number} onChange={(e)=> setNumber(e.target.value)} type="number" placeholder='عدد رو تغییر بده' style={{ fontSize: "17px", padding: "5px 10px", width: "100%", margin: "0", color: "GrayText" }} />
+        <button onClick={()=> setColor(!color)} style={{ fontSize: "16px", padding: "5px 50px", textAlign: "center", margin: "20px 0", outline: "none", background: "blue", color: "white" }}>
+          تغییر رنگ
+        </button>
+      </form>
+      <div style={{ width: "500px", textAlign: "center", padding: "10px 30px", margin: "auto", background: appstyle.background, transition:"all ease .5s" }} >
+        عدد دوبرابر شده: {supernum} است!
+      </div>
     </>
-   );
+  );
 }
- 
+
 export default MemoExample;
